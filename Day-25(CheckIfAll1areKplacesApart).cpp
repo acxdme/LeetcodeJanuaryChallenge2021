c1e7c3300a6584566ca65c96926988e2a759e897
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2021-01-25 22:17:32 +0530

    Update Day-25(CheckIfAll1areKplacesApart).cpp

--- Day-25(CheckIfAll1areKplacesApart).cpp
+++ Day-25(CheckIfAll1areKplacesApart).cpp
@@ -1,6 +1,29 @@
 /*
-Time complexity :
-Space complexity :
-My approach :
+Time complexity : O(N)
+Space complexity : O(1)
+My approach : check distance between each one occuring is sufficient than k or not.
 */
+
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int place = 0;
+        int n = nums.size();
+        bool fst = false;
+        for(int i=0;i<n;i++)
+            if(nums[i] == 1 and fst == false)
+            {
+                fst =true;
+                place = i;
+            }
+           else if( nums[i] == 1 and fst == true)
+           {
+                if( i-place < k+1) return false;
+                place = i;
+           }
+        
+        return true;
+        
+    }
+};
 
